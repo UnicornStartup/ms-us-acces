9cd6dd787607794d41e25e8391467147b6846ccc
Willdune <ateixidof@gmail.com>
2022-03-23 16:41:12 +0100

    Modificaciones
    Modificaciones para trabajar desde el portatil

---
+++ dist/models/LoginRequestBodyView.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LoginRequestBodyView = void 0;
+class LoginRequestBodyView {
+    constructor(email, password) {
+        this.email = email;
+        this.password = password;
+    }
+}
+exports.LoginRequestBodyView = LoginRequestBodyView;
--- src/routes/login.route.ts
+++ src/routes/login.route.ts
@@ -1,10 +1,10 @@
 import {Router} from 'express'
 import {
-  register
+  login
 } from '../controllers/login.controller'
 
 const router = Router();
 
-router.post('/register', register);
+router.get('/login', login);
 
 export default router;
