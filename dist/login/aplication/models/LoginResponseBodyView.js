d8eaf87d980ef749f4bd2a71d691e1d924154ec0
Willdune <ateixidof@gmail.com>
2022-03-25 23:34:31 +0100

    WIP

--- dist/login/aplication/models/LoginResponseBodyView.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoginResponseBodyView = void 0;
-class LoginResponseBodyView {
-}
-exports.LoginResponseBodyView = LoginResponseBodyView;
--- dist/routes/login.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const login_controller_1 = require("../controllers/login.controller");
-const router = (0, express_1.Router)();
-router.post('/register', login_controller_1.register);
-exports.default = router;
