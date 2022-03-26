06645955f3ba12c658f7fa6009a0af83eb9a92e6
Willdune <ateixidof@gmail.com>
2022-03-27 00:04:46 +0100

    WIP 1

--- src/integration-tests/login-integration-tests/LoginControllerTests.ts
+++ src/integration-tests/login-integration-tests/LoginControllerTests.ts
@@ -1,11 +1,6 @@
 import { expect } from 'chai';
 import 'mocha';
 import { mock, when, anything, anyNumber, anyString, anyOfClass, instance, anyFunction, between, objectContaining } from 'ts-mockito';
-import { LoginAdapter } from '../../main/login/aplication/adapters/LoginAdapter';
-import { LoginRequestBodyView } from '../../main/login/aplication/models/LoginRequestBodyView';
-import { LoginResponseBodyView } from '../../main/login/aplication/models/LoginResponseBodyView';
-import { User } from "../../main/login/domain/models/User";
-import { UserBuilder } from '../../main/login/domain/models/UserBuilder';
 
 const mockedFoo: LoginRequestBodyView = mock(LoginRequestBodyView);
 
