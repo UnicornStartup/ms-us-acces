ca330fec5f18cf6b7266cee2df0ff0647c7e8d6f
Willdune <ateixidof@gmail.com>
2022-04-07 00:36:06 +0200

    WIP

--- src/unit-tests/login-unit-tests/LoginControllerTests.ts
+++ src/unit-tests/login-unit-tests/LoginControllerTests.ts
@@ -1,24 +1,62 @@
-import { assert, expect } from "chai";
+import { assert } from "chai";
+import cors from "cors";
+import express from "express";
 import "reflect-metadata";
 import { mock, when, instance } from "ts-mockito";
-import app from '../../main/app';
+import LoginAdapter from "../../main/login/aplication/adapters/LoginAdapter";
+import LoginController from "../../main/login/aplication/Controller/LoginController";
+import { LoginResponseBodyView } from "../../main/login/aplication/models/LoginResponseBodyView";
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const app = express();
+const loginAdapter = mock(LoginAdapter);
 
-const URL = "http://localhost:3000";
 const LOGIN_PATH = "/login";
 const SOME_EMAIL = "email@email.com";
+const SOME_PASSWORD = "password"
+const SOME_TOKEN = "2a3260e6-b44b-11ec-b909-0242ac120002";
 const SOME_STATUS: number = 400;
 
 describe("LoginController", async () => {
-    it("Should return response with error 'error parsing request body' when email or password are missing", async () => {
+    before(() => {
+        console.log("me ejecuto before");
+        app.use(cors());
+        app.use(express.urlencoded({ extended: false }));
+        app.use(express.json());
+        app.listen(3001);
+        const loginController = new LoginController(instance(loginAdapter));
+        app.use('/login', loginController.routes());
+    });
 
+    it("Should return response with error 'error parsing request body' when email or password are missing", async () => {
         chai.use(chaiHttp);
         let result = await chai.request(app)
             .get(LOGIN_PATH)
             .send({ email: SOME_EMAIL });
 
         assert.equal(result.status, SOME_STATUS);
+    });
+
+    it("Should return response with token when email and password are in body, and user in database exists", async () => {
+        when(loginAdapter.adapt(SOME_EMAIL, SOME_PASSWORD)).thenResolve(new LoginResponseBodyView(SOME_TOKEN));
+        chai.use(chaiHttp);
+        let result = await chai.request(app)
+            .get(LOGIN_PATH)
+            .send({ email: SOME_EMAIL, password: SOME_PASSWORD });
+
+        console.log(result.body);
+        assert.equal(result.body.token, SOME_TOKEN);
+    });
+
+    it("asdf", async () => {
+        when(loginAdapter.adapt(SOME_EMAIL, SOME_PASSWORD)).thenResolve(new LoginResponseBodyView(SOME_TOKEN));
+        chai.use(chaiHttp);
+        let result = await chai.request(app)
+            .get(LOGIN_PATH)
+            .send({ email: SOME_EMAIL, password: SOME_PASSWORD });
+
+        console.log(result.body);
+        assert.equal(result.body.token, SOME_TOKEN);
     });
 });
