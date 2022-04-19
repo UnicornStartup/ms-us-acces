643ecdcbcf43abdb7188c7c982a8f2d5d5bab17a
Willdune <ateixidof@gmail.com>
2022-04-19 19:48:54 +0200

    WIP

--- src/main/shared/infraestructure/LogServiceOpenSearch.ts
+++ src/main/shared/infraestructure/LogServiceOpenSearch.ts
@@ -1,9 +1,37 @@
 import { LogModel } from "../domain/models/LogModel";
 import { LogService } from "../domain/services/LogService";
+import { Client } from "@opensearch-project/opensearch";
+import fs from "fs";
+
+var host = 'localhost';
+var protocol = 'https';
+var port = 9200;
+var auth = 'admin:admin';
+var ca_certs_path = '/full/path/to/root-ca.pem';
+
+
+var client = new Client({
+    node: protocol + '://' + auth + '@' + host + ':' + port,
+    ssl: {
+        ca: fs.readFileSync(ca_certs_path),
+    }
+})
 
 export class LogServiceOpenSearch implements LogService{
-    error(logModel: LogModel) {
-        throw new Error("Method not implemented.");
+    async error(logModel: LogModel) {
+        let document = generateDocument(logModel);
+        var id = '1'
+    
+        var response = await client.index({
+            id: id,
+            index: "index_name",
+            body: document,
+            refresh: true
+        });
+    
+        console.log('Adding document:');
+        console.log(response.body);
+    
     }
     warning(logModel: LogModel) {
         throw new Error("Method not implemented.");
@@ -15,4 +43,13 @@ export class LogServiceOpenSearch implements LogService{
         throw new Error("Method not implemented.");
     }
 
+    
+}
+
+function generateDocument(logModel: LogModel) {
+    return {
+        'logType': logModel.logType,
+        'trace': logModel.trace,
+        'className': logModel.className
+    }
 }
